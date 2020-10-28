4025c9301783a214e6fd0136f2b884a77e3ca6e4
LukasBek <lukas@starsite.dk>
2020-10-28 22:57:28 +0100

    added login function and basic accesstoken to printer side

--- src/interfaces/PrinterServiceInterface.java
+++ src/interfaces/PrinterServiceInterface.java
@@ -5,22 +5,24 @@ import java.rmi.RemoteException;
 
 public interface PrinterServiceInterface extends Remote{
 
-	public boolean print(String filename, String printer) throws RemoteException;   // prints file filename on the specified printer
+	public String login(String username, String password) throws RemoteException;
+
+	public boolean print(String filename, String printer, String accessToken) throws RemoteException;   // prints file filename on the specified printer
 	
-	public String queue(String printer) throws RemoteException;   // lists the print queue for a given printer on the user's display in lines of the form <job number>   <file name>
+	public String queue(String printer, String accessToken) throws RemoteException;   // lists the print queue for a given printer on the user's display in lines of the form <job number>   <file name>
 	
-	public boolean topQueue(String printer, int job) throws RemoteException;   // moves job to the top of the queue
+	public boolean topQueue(String printer, int job, String accessToken) throws RemoteException;   // moves job to the top of the queue
 	
-	public boolean start() throws RemoteException;   // starts the print server
+	public boolean start(String accessToken) throws RemoteException;   // starts the print server
 	
-	public boolean stop() throws RemoteException;   // stops the print server
+	public boolean stop(String accessToken) throws RemoteException;   // stops the print server
 	
-	public boolean restart() throws RemoteException;   // stops the print server, clears the print queue and starts the print server again
+	public boolean restart(String accessToken) throws RemoteException;   // stops the print server, clears the print queue and starts the print server again
 	
-	public String status(String printer) throws RemoteException;  // prints status of printer on the user's display
+	public String status(String printer, String accessToken) throws RemoteException;  // prints status of printer on the user's display
 	
-	public String readConfig(String parameter) throws RemoteException;   // prints the value of the parameter on the user's display
+	public String readConfig(String parameter, String accessToken) throws RemoteException;   // prints the value of the parameter on the user's display
 	
-	public boolean setConfig(String parameter, String value) throws RemoteException;   // sets the parameter to value
+	public boolean setConfig(String parameter, String value, String accessToken) throws RemoteException;   // sets the parameter to value
 	
 }
--- src/server/PrinterServant.java
+++ src/server/PrinterServant.java
@@ -12,55 +12,100 @@ public class PrinterServant extends UnicastRemoteObject implements PrinterServic
 	}
 
 	@Override
-	public boolean print(String filename, String printer) throws RemoteException {
+	public String login(String username, String password) throws RemoteException {
+		if (PrinterServer.authenticate(username, password)) {
+			// TODO SEBASTIAN TOKENS
+			return "SomeRandomAccessToken";
+		}
+		return "";
+	}
+
+	@Override
+	public boolean print(String filename, String printer, String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS (maybe pull all these calls to service?
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Client requested "+filename+" on printer: "+printer);
 		return true;
 	}
 
 	@Override
-	public String queue(String printer) throws RemoteException {
+	public String queue(String printer, String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Client requested the queue on following printer: "+printer);
 		return "Printer queue data.";
 	}
 
 	@Override
-	public boolean topQueue(String printer, int job) throws RemoteException {
+	public boolean topQueue(String printer, int job, String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Putting job #"+job+" at the top on printer: "+printer);
 		return true;
 	}
 
 	@Override
-	public boolean start() throws RemoteException {
+	public boolean start(String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Starting the printer service.");
 		return true;
 	}
 
 	@Override
-	public boolean stop() throws RemoteException {
+	public boolean stop(String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Stopping the printer service.");
 		return true;
 	}
 
 	@Override
-	public boolean restart() throws RemoteException {
+	public boolean restart(String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Restarting the printer service.");
 		return true;
 	}
 
 	@Override
-	public String status(String printer) throws RemoteException {
+	public String status(String printer, String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Client requested status of printer: "+printer);
 		return "Status of printer data.";
 	}
 
 	@Override
-	public String readConfig(String parameter) throws RemoteException {
+	public String readConfig(String parameter, String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Client requested to read config of parameter.");
 		return "Config parameter data.";
 	}
 
 	@Override
-	public boolean setConfig(String parameter, String value) throws RemoteException {
+	public boolean setConfig(String parameter, String value, String accessToken) throws RemoteException {
+		// TODO SEBASTIAN TOKENS
+		if(accessToken != "checksomerandom accessToken") {
+			throw new RemoteException("Access not allowed");
+		}
 		System.out.println("Client setting config parameter: "+parameter+" with value: "+value);
 		return true;
 	}
