6d807f39383f57fd9a595d80732734240fb5c577
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-11-18 12:57:04 +0100

    fix hierarchy problem partially

--- src/server/AuthorizationByRole.java
+++ src/server/AuthorizationByRole.java
@@ -17,16 +17,33 @@ public class AuthorizationByRole {
         /*
          * Loads all roles, permissions and users associated to roles.
          */
+        permissions = new HashMap<String, HashSet<String>>();
+        hierarchy = new HashMap<String, HashSet<String>>();
+        userRole = new HashMap<String, String>();
         File file = new File("ROLES_DESCRIPTION.txt");
         Scanner reader = new Scanner(file);
         while (reader.hasNextLine()) {
-            String[] data = reader.nextLine().split(" | ");
+            String[] data = reader.nextLine().split(":");
             HashSet<String> parents = new HashSet<String>(Arrays.asList(data[1].split(" ")));
             hierarchy.put(data[0], parents);
             HashSet<String> roleRights = new HashSet<String>(Arrays.asList(data[2].split(" ")));
             permissions.put(data[0], roleRights);
         }
+        for (String child : permissions.keySet()) {
+            System.out.print(child + " : ");
+            for (String parent : permissions.get(child)) {
+                System.out.print(parent + " ");
+            }
+            System.out.println("");
+        }
         reader.close();
+        for (String child : hierarchy.keySet()) {
+            System.out.print(child + " : ");
+            for (String parent : permissions.get(child)) {
+                System.out.print(parent + " ");
+            }
+            System.out.println("");
+        }
         for (String role : permissions.keySet()) {
             exploreParentPermissions(role);
         }
@@ -38,6 +55,14 @@ public class AuthorizationByRole {
             userRole.put(line[0], line[1]);
         }
         reader.close();
+        for (String child : permissions.keySet()) {
+            System.out.print(child + " : ");
+            for (String parent : permissions.get(child)) {
+                System.out.print(parent + " ");
+            }
+            System.out.println("");
+        }
+
     }
 
     public HashSet<String> exploreParentPermissions(String childRoleName) {
@@ -45,20 +70,24 @@ public class AuthorizationByRole {
          * Recursively iterate through the parents of a role to build all its
          * permissions.
          */
-        if (this.hierarchy.get(childRoleName).size() == 0) {
-            return this.permissions.get(childRoleName);
-        } else {
-            HashSet<String> additionalPermissions = new HashSet<String>();
-            if (this.permissions.get(childRoleName).size() > 0) {
-                additionalPermissions.addAll(this.permissions.get(childRoleName));
+        if (this.hierarchy.get(childRoleName) != null) {
+            if (this.hierarchy.get(childRoleName).size() == 0) {
+                return this.permissions.get(childRoleName);
+            } else {
+                HashSet<String> additionalPermissions = new HashSet<String>();
+                if (this.permissions.get(childRoleName).size() > 0) {
+                    additionalPermissions.addAll(this.permissions.get(childRoleName));
+                }
+
+                for (String parent : this.hierarchy.get(childRoleName)) {
+                    this.hierarchy.get(childRoleName).remove(parent);
+                    additionalPermissions.addAll(exploreParentPermissions(parent));
+                    this.permissions.get(childRoleName).addAll(additionalPermissions);
+                }
+                return additionalPermissions;
             }
-            for (String parent : this.hierarchy.keySet()) {
-                this.hierarchy.get(parent).remove(parent);
-                additionalPermissions.addAll(exploreParentPermissions(parent));
-                this.permissions.get(parent).addAll(additionalPermissions);
-            }
-            return additionalPermissions;
         }
+        return new HashSet<String>();
     }
 
     public boolean checkPermission(String functionName, String role) {
